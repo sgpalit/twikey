3d5df6afbc178b084078a6f311f922cb5b9c16b2
Seydi Palit <sgpalit@gmail.com>
2025-10-07 19:07:05 +0200

    Initialize Spring Boot project with Maven Wrapper, basic configurations, and required dependencies.

---
+++ src/main/java/be/palit/twikey/TwikeyApplication.java
@@ -0,0 +1,13 @@
+package be.palit.twikey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwikeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwikeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/palit/twikey/TwikeyApplicationTests.java
@@ -0,0 +1,13 @@
+package be.palit.twikey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwikeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
